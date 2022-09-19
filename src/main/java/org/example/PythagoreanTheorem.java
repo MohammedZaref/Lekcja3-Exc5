285c2ad9c4ad7a9279da933af529a67820cb1fb4
MohammedZaref <melkady254@gmail.com>
2022-09-19 21:03:54 +0100

    Created a method to check if the hypotenuse sqr is equal to the sum of base and height sqrs

---
+++ src/main/java/org/example/PythagoreanTheorem.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class PythagoreanTheorem {
+    public static boolean isRight(int base, int height, int hypotenuse) {
+        boolean isRightTriangle = false;
+        if (hypotenuse * hypotenuse == (base * base) + (height * height)) {
+            isRightTriangle = true;
+            return isRightTriangle;
+        } else {
+            return isRightTriangle;
+        }
+    }
+}
