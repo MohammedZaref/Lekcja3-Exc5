7c474527b6c95eb81d6421c9a65d6feb8b3fe2d7
MohammedZaref <melkady254@gmail.com>
2022-09-19 21:11:04 +0100

    Converted the result of isRight to an understandable message for user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // CREATE A METHOD TO CHECK IF GIVEN VALUSE OF 3 SIDES OF TRIANGLES WILL RESULT A TRIANGLE WITH AN RIGHT ANGLE
+        // CREATE A METHOD TO CHECK IF GIVEN VALUES OF 3 SIDES OF TRIANGLES WILL RESULT A TRIANGLE WITH AN RIGHT ANGLE
         // BASED ON PYTHAGOREAN THEOREM IF THE SQR OF HYPOTENUSE IS EQUAL TO THE SUM OF THE OTHER TWO SIDES SQR SO THE TRIANGLE IS A RIGHT ANGLE TRIANGLE
         // PYTHAGOREAN EQUATION: A^ = B^2 + C^2
 
@@ -17,5 +17,12 @@ public class Main {
                 sc.nextInt(), sc.nextInt(), sc.nextInt()
         };
         Arrays.sort(triangleThreeSides);
+
+        boolean isRightContainer = PythagoreanTheorem.isRight(triangleThreeSides[0], triangleThreeSides[1], triangleThreeSides[2]);
+        if (isRightContainer) {
+            System.out.println("The Triangle has a Right Angle");
+        } else {
+            System.out.println("The Triangle doesn't have any Right Angle");
+        }
     }
 }
