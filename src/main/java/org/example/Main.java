3d55ab8d533270774833d4562ce88c8b47ef0cee
MohammedZaref <melkady254@gmail.com>
2022-09-19 21:01:19 +0100

    Created a scanner and an array to take 3 inputs from the user as base, height and hypotenuse of a triangle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // CREATE A METHOD TO CHECK IF GIVEN VALUSE OF 3 SIDES OF TRIANGLES WILL RESULT A TRIANGLE WITH AN RIGHT ANGLE
         // BASED ON PYTHAGOREAN THEOREM IF THE SQR OF HYPOTENUSE IS EQUAL TO THE SUM OF THE OTHER TWO SIDES SQR SO THE TRIANGLE IS A RIGHT ANGLE TRIANGLE
         // PYTHAGOREAN EQUATION: A^ = B^2 + C^2
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input three integer numbers (Base, Height and Hypotenuse of Triangle) separated with a space: ");
+        int[] triangleThreeSides = new int[]{
+                sc.nextInt(), sc.nextInt(), sc.nextInt()
+        };
     }
 }
